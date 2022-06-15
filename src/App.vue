0e1c0617b6a6d78888b5b205e122b51e0ddeeb24
kchyzhyk <kchyzhykova888@gmail.com>
2022-06-16 00:58:25 +0200

    initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,24 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="container-fluid bg-black vh-100 mw-100">
+    <div class="col-12 d-flex h-100 justify-content-center align-items-lg-center align-items-md-center align-items-start flex-column px-4 px-lg-0 px-md-0">
+      <div class="col-9 col-md-6 col-lg-3">
+        <img class="w-100" src="../src/assets/logo-why.svg" alt="">
+      </div>
+      <div class="col-12 col-sm-10 col-md-10 col-lg-10 mt-lg-5 mt-md-3 mt-sm-3 mt-5">
+        <div class="text-white text-lg-center text-md-center text-sm-center text-start">This website may be an answer to your questions about Ukraine. <br> Currently, it is in development.</div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 </style>
