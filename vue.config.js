4d3fc7c4ad554fffd121752331034c1972aeea38
kchyzhyk <kchyzhykova888@gmail.com>
2022-06-21 13:04:14 +0200

    initial commit

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,5 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
-  publicPath: process.env.NODE_ENV === "production" ? "/ukraine/" : "/",
+  publicPath: process.env.NODE_ENV === "production" ? "/ukraine" : "/",
 })
